f42c8bc4e30b37c21907a542e98c6653638c928f
serious <seriouz@gmail.com>
2024-01-24 00:07:22 +0100

    Create the folders for the Sqlite blob files if they do not exist.
    As mentioned in bagetter/BaGette#38 under some special configurations Sqlite failed with a "folder not found" exception.

--- src/BaGetter.Database.Sqlite/SqliteContext.cs
+++ src/BaGetter.Database.Sqlite/SqliteContext.cs
@@ -1,3 +1,8 @@
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 using BaGetter.Core;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
@@ -6,6 +11,8 @@ namespace BaGetter.Database.Sqlite
 {
     public class SqliteContext : AbstractContext<SqliteContext>
     {
+        private static readonly Regex WindowsDriveLetterRegex = new("^[A-Za-z]:", RegexOptions.Compiled);
+
         /// <summary>
         /// The Sqlite error code for when a unique constraint is violated.
         /// </summary>
@@ -44,6 +51,76 @@ namespace BaGetter.Database.Sqlite
             builder.Entity<TargetFramework>()
                 .Property(f => f.Moniker)
                 .HasColumnType("TEXT COLLATE NOCASE");
+        }
+
+        public override async Task RunMigrationsAsync(CancellationToken cancellationToken)
+        {
+            if (Database.GetDbConnection() is SqliteConnection connection)
+            {
+                /* Create the folder of the Sqlite blob if it does not exist. */
+                CreateSqliteDataSourceDirectory(connection);
+            }
+
+            await base.RunMigrationsAsync(cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates directories specified in the Database::ConnectionString config for the Sqlite database file.
+        /// Respects the difference between Windows and other OS root or relative paths.
+        /// </summary>
+        /// <param name="connection">Instance of the <see cref="SqliteConnection"/>.</param>
+        private static void CreateSqliteDataSourceDirectory(SqliteConnection connection)
+        {
+            var configIsLinuxRootPath = connection.DataSource.StartsWith('/');
+            var configDataSource = connection.DataSource;
+
+            string? pathToCreate = null;
+
+            if (OperatingSystem.IsWindows())
+            {
+                var windowsDriveLetter = Path.GetPathRoot(Directory.GetCurrentDirectory())!;
+
+                // root path
+                if (configIsLinuxRootPath)
+                {
+                    var directories = Path.GetDirectoryName(configDataSource);
+                    if (directories?.Length > 0) pathToCreate = Path.Combine(windowsDriveLetter, directories);
+                }
+                else if (WindowsDriveLetterRegex.IsMatch(configDataSource))
+                {
+                    pathToCreate = Path.GetDirectoryName(configDataSource);
+                }
+                else
+                {
+                    pathToCreate = CreateFullDataSourcePath();
+                }
+            }
+            else
+            {
+                pathToCreate = configIsLinuxRootPath
+                    // root path
+                    ? Path.GetDirectoryName(configDataSource)
+                    // relative path
+                    : CreateFullDataSourcePath();
+            }
+
+            if (pathToCreate is not null)
+            {
+                Directory.CreateDirectory(pathToCreate);
+            }
+
+            return;
+
+            string CreateFullDataSourcePath()
+            {
+                var exeDir = Directory.GetCurrentDirectory();
+                var relativePath = Path.GetDirectoryName(configDataSource);
+                var relativeDirectoriesArray = relativePath?.Split(Path.DirectorySeparatorChar) ?? [];
+                var relativeDirectoriesCombined = Path.Combine(relativeDirectoriesArray);
+                var fullPath = Path.Combine(exeDir, Path.Combine(relativeDirectoriesCombined));
+
+                return fullPath;
+            }
         }
     }
 }
